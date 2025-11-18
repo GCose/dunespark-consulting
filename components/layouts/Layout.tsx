5e0cbd26a30502491ede400542a30ce04d93f7e3
GCose <goodnessadewuyi8@gmail.com>
2025-11-18 08:57:25 +0000

    Made changes

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,14 +1,25 @@
+import Head from "next/head";
 import Footer from "./Footer";
-import Navigation from "./Navigation";
 import { LayoutProps } from "@/types";
+import Navigation from "./Navigation";
 
-const Layout = ({ children }: LayoutProps) => {
+const Layout = ({
+  children,
+  title = "Dunespark Consulting",
+  description = "Install elegant, AI-powered growth systems that generate leads, close deals, and scale your business effortlessly.",
+}: LayoutProps) => {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Navigation />
-      <main className="flex-grow pt-28">{children}</main>
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Head>
+      <div className="min-h-screen flex flex-col px-4">
+        <Navigation />
+        <main className="flex-grow pt-32">{children}</main>
+      </div>
       <Footer />
-    </div>
+    </>
   );
 };
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,24 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
+const Document = () => {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
+        />
+        <link
+          href="https://api.fontshare.com/v2/css?f[]=epilogue@100,101,200,201,300,301,400,401,500,501,600,601,700,701,800,801,900,901&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
     </Html>
   );
-}
+};
+
+export default Document;
